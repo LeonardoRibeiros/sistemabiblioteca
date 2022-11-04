6e5cef277d61fa6230b896c668318fb1150b9d7d
Leonardo <leo.ribeiros2014@gmail.com>
2022-11-04 11:32:43 -0300

    commit 04/11/22

---
+++ src/controle/ControleUsuario.java
@@ -0,0 +1,29 @@
+package controle;
+
+import java.util.ArrayList;
+
+import modelo.Usuario;
+
+public class ControleUsuario {
+private ArrayList<Usuario> tabelausuario;
+public ControleUsuario() {this.tabelausuario = new ArrayList<>();}
+
+public boolean insert(Usuario u) {
+	
+	return false;
+}
+public boolean alterar(Usuario u) {
+	
+	return false;
+}
+public boolean deletar(Usuario u) {
+	
+	return false;
+}
+public ArrayList<Usuario> listaUsuario() {
+	
+	return null;
+}
+
+
+}
